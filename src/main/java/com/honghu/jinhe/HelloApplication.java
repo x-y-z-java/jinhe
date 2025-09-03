ad1c6b24e4dac3510b590b140d92ba3a21a53bef
xyz <634604898@qq.com>
2025-09-03 17:59:02 +0800

    项目启动了

--- src/main/java/com/honghu/jinhe/HelloApplication.java
+++ src/main/java/com/honghu/jinhe/HelloApplication.java
@@ -6,11 +6,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+        // 添加调试代码开始
+        URL resourceUrl = getClass().getClassLoader().getResource("com/honghu/jinhe/hello-view.fxml");
+
+        FXMLLoader fxmlLoader = new FXMLLoader( getClass().getClassLoader().getResource("com/honghu/jinhe/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
